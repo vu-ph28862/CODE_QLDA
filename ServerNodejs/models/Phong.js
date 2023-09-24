403e76222b9ec9946aba729781aa00103832aa4c
NgoThiNguyetHa <hantnph28876@fpt.edu.vn>
2023-09-24 21:12:12 +0700

    api phòng

---
+++ ServerNodejs/models/Phong.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+
+const SachSchema = mongoose.Schema({
+    maLoaiPhong:{type: mongoose.Schema.Types.ObjectId, ref: 'loaiPhong'},  // const LoaiPhong = mongoose.model("loaiPhong");
+    tenPhong:{type: String},
+    tinhTrang: {type: String},
+    tienThueTheoGio: {type: String},
+    tienThueTheoNgay:{type: String}
+});
+module.exports = mongoose.model('phong', SachSchema);
