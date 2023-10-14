84ecd6153b765884766536d5c6d31f667587786f
HoangLong <hoanglong180903@gmail.com>
2023-10-14 14:05:52 +0700

    fix loi 14_10

--- Screen/HoaDonScreen.js
+++ Screen/HoaDonScreen.js
@@ -1,12 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function LoaiDichVu() {
+export default function HoaDonScreen() {
   return (
     <View>
-      <Text>LoaiDichVu</Text>
+      <Text>HoaDon</Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  
+})
